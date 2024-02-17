9e80b6e75fedb7684514f122f0ac45b8d6cb1b02
vini-fmo <vinicius.fmo16@gmail.com>
2024-02-16 21:16:38 -0300

    create users, tasks and tags CRUDs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [],
+  imports: [TasksModule, UsersModule, TagsModule],
   controllers: [AppController],
   providers: [AppService],
 })
