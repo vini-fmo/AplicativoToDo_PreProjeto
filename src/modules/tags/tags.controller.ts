b17bb23a8d1966ccb80109dde8950ec6ef62bf76
vini-fmo <vinicius.fmo16@gmail.com>
2024-02-17 14:58:09 -0300

    create new routes in tags controller

--- src/modules/tags/tags.controller.ts
+++ src/modules/tags/tags.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+
+import { Controller, Post } from '@nestjs/common';
 import { TagsService } from './tags.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('tags')
 export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
+
+  @Post(){
+    async create(data: Prisma.TagCreateInput) {
+      
+    }
+  }
 }
